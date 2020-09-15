bf378fdd1e76d8d1981e7e3a2d2f66aa9064fdab
lkaine24 <lkaine24@gmail.com>
2020-09-15 11:35:36 -0700

    Resubmission for Week 2 VPN configuration assignment

--- Week02/homework/server.bash
+++ Week02/homework/server.bash
@@ -2,6 +2,24 @@
 
 # Storyline: Script to create a wireguard server
 
+# Create a private key
+p="$(wg genkey)"
+
+# Create a public key
+pub="$(echo ${p} | wg pubkey)"
+
+# Set the addresses
+address="10.254.132.0/24,172.16.28.0/24"
+
+# Set Server IP addresses
+ServerAddress="10.254.132.1/24,172.16.28.1/24"
+
+# Set the Listen port
+lport="4282"
+
+# Create the format for the client configuration options
+peerInfo="# ${address} 198.199.97.163:4282 ${pub} 8.8.8.8,1.1.1.1 1280 120 0.0.0.0/0"
+
 # Check if the server configuration file exists
 
 if [[ -f "wg0.conf" ]]
@@ -19,24 +37,6 @@ then
         then
                 echo "Creating the wireguard configuration file..."
 
-# Create a private key
-p="$(wg genkey)"
-
-# Create a public key
-pub="$(echo ${p} | wg pubkey)"
-
-# Set the addresses
-address="10.254.132.0/24,172.16.28.0/24"
-
-# Set Server IP addresses
-ServerAddress="10.254.132.1/24,172.16.28.1/24"
-
-# Set the Listen port
-lport="4282"
-
-# Create the format for the client configuration options
-peerInfo="# ${address} 198.199.97.163:4282 ${pub} 8.8.8.8,1.1.1.1 1280 120 0.0.0.0/0"
-
 echo "${peerInfo}
 [Interface]
 Address = ${ServerAddress}
@@ -52,24 +52,6 @@ exit 0
                 exit 1
         fi
 fi
-
-# Create a private key
-p="$(wg genkey)"
-
-# Create a public key
-pub="$(echo ${p} | wg pubkey)"
-
-# Set the addresses
-address="10.254.132.0/24,172.16.28.0/24"
-
-# Set Server IP addresses
-ServerAddress="10.254.132.1/24,172.16.28.1/24"
-
-# Set the Listen port
-lport="4282"
-
-# Create the format for the client configuration options
-peerInfo="# ${address} 198.199.97.163:4282 ${pub} 8.8.8.8,1.1.1.1 1280 120 0.0.0.0/0"
 
 echo "${peerInfo}
 [Interface]
